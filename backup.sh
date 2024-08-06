77f507d5c7087315ba0fba47f87bbe2ba20d8e46
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-08-06 16:48:24 +0100

    use backup timestamps in folders rather than file names

--- backup.sh
+++ backup.sh
@@ -2,11 +2,13 @@
 
 TIMESTAMP=`date +%F\T%T\Z`
 
+mkdir -p backups/${TIMESTAMP}
+
 docker run --rm \
     -v odoo-data:/odoo-data \
     -v "$(pwd)":/mount \
     busybox \
-    tar -zcf /mount/backups/odoo-data-${TIMESTAMP}.tar.gz ./odoo-data
+    tar -zcf /mount/backups/${TIMESTAMP}/odoo-data.tar.gz ./odoo-data
 
-tar -zcf backups/odoo-addons-${TIMESTAMP}.tar.gz ./addons
-tar -zcf backups/odoo-config-${TIMESTAMP}.tar.gz ./config
+tar -zcf backups/${TIMESTAMP}/odoo-addons.tar.gz ./addons
+tar -zcf backups/${TIMESTAMP}/odoo-config.tar.gz ./config
