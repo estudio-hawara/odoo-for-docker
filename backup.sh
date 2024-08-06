49b872ff6d07ecbeabb90968b2dbb3b1a1da4604
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-08-06 16:55:24 +0100

    simplify the backup folder by removing timestamps

--- backup.sh
+++ backup.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-TIMESTAMP=`date +%F\T%T\Z`
-
-mkdir -p backups/${TIMESTAMP}
-
 docker run --rm \
     -v odoo-data:/odoo-data \
     -v "$(pwd)":/mount \
     busybox \
-    tar -zcf /mount/backups/${TIMESTAMP}/odoo-data.tar.gz ./odoo-data
+    tar -zcf /mount/backups/odoo-data.tar.gz ./odoo-data
 
-tar -zcf backups/${TIMESTAMP}/odoo-addons.tar.gz ./addons
-tar -zcf backups/${TIMESTAMP}/odoo-config.tar.gz ./config
+tar -zcf backups/odoo-addons.tar.gz ./addons
+tar -zcf backups/odoo-config.tar.gz ./config
