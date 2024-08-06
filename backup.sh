d93cdaa9c0e4f405f21b7349df36e1e6b46ae5c6
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2024-08-06 17:17:32 +0100

    create a separate file with the last backup timestamp

--- backup.sh
+++ backup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+source .env
+
 docker run --rm \
-    -v odoo-data:/odoo-data \
+    -v ${VOLUME_NAME}:/odoo-data \
     -v "$(pwd)":/mount \
     busybox \
     tar -zcf /mount/backups/odoo-data.tar.gz ./odoo-data
 
 tar -zcf backups/odoo-addons.tar.gz ./addons
 tar -zcf backups/odoo-config.tar.gz ./config
+
+echo "The last backup was made at: `date +%F\T%T\Z`" > backups/odoo-lastbackup.info
